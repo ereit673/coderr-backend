9619f565008da5d7f56779424839b8a52bbed82a
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-08-08 16:35:47 +0200

    Start marketplace app

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('users_app.api.urls'))
+    path('api/', include('users_app.api.urls')),
+    path('api/', include('marketplace_app.api.urls')),
 ]

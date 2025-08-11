9d927c8597310a00819450411f1b9fec48ba0e66
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-08-11 02:46:09 +0200

    Fix IsBusiness permission

--- marketplace_app/api/permissions.py
+++ marketplace_app/api/permissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsBusiness(BasePermission):
     def has_permission(self, request, view):
         # Check if the user is authenticated and has a business profile
-        return request.user.is_authenticated and request.user.profile.is_business
+        return request.user.is_authenticated and request.user.type == 'business'
